9c849841116c7414e3e74eae25470dfda4b3137d
_kbluue_ <kbluue@yahoo.com>
2018-02-23 14:18:36 +0100

    safe

---
+++ imports/ui/body.js
@@ -0,0 +1,20 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+import { Users } from '../api/tasks.js';
+ 
+import './body.html';
+ 
+Template.body.helpers({
+  myTasks: [
+    { text: 'This is task 1' },
+    { text: '8'},
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+  equals8: function(a) {
+      return a == '8';
+  },
+  mongoTasks() {
+      return Tasks.find({}) && Users.find({});
+  }
+});
